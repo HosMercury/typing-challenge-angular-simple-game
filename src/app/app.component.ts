56185815274bd70b4bcdd66b3fef7fa1e7629b7a
Hossam Maher <hossam@Hossams-MacBook-Pro.local>
2021-01-23 02:34:24 +0200

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {lorem} from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+  text =''
+  randomText  = lorem.sentence();
+
+  onInput(value: string){
+    this.text = value;
+  }
+
+  getClass(randomLetter: string , textLetter: string){
+    if(!textLetter) return 'pending'
+
+   return randomLetter== textLetter? 'green': 'red';
+  }
+
 }
